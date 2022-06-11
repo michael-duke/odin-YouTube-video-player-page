6289d163a96bd64e49211a108fc3382d8d739013
michael-duke <michael.21duke@gmail.com>
2022-06-11 16:10:21 +0300

    Add active class, Add header, Add video desc

---
+++ js/active-button.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+  addActiveClass()
+});
+
+const addActiveClass = ()=>{
+  const header = document.getElementById("related-topics");
+  const btns = header.getElementsByClassName("topic-btn");  
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("topic-active");
+    current[0].className = current[0].className.replace(" topic-active", "");
+    this.className += " topic-active";
+    });
+  }
+}
